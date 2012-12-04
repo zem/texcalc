d08536599f96866dbc808bd0a92c02e7293de674
Hans Freitag <zem@nawi.at>
2012-12-04 17:15:12 +0100

    neue development version
    kann vielleicht @var@

--- texcalc.pl
+++ texcalc.pl
@@ -4,8 +4,28 @@ use IPC::Open3;
 use IO::Handle;
 use IO::Select; 
 
-open(READ, "< $ARGV[0]") or die "READ Opemn fail"; 
-open(WRITE, "> .$ARGV[0].tmp") or die "Write open fail"; 
+my $leavetexfile; 
+my $pdflatex; 
+my $texfile; 
+while (my $arg=shift(@ARGV)) {
+	if ($arg eq "-c") {
+		# -compile
+		$pdflatex="pdflatex"; 
+	} elsif ($arg eq "-C") {
+		if (shift(@ARGV) eq "-y") {
+			# -compile
+			$pdflatex="pdflatex"; 
+			$leavetexfile=1;
+		} else { 
+			die "-C must be followed by -y\n"; 
+		}
+	} else { 
+		$texfile=$arg;
+	}
+}
+
+open(READ, "< $texfile") or die "READ Opemn fail"; 
+open(WRITE, "> .$texfile.tmp") or die "Write open fail"; 
 
 my $pid = open3(\*CALC_IN, \*CALC_OUT, \*CALC_ERR, 'calc.py -t')
     or die "open3() failed $!";
@@ -19,6 +39,45 @@ CALC_ERR->autoflush(1);
 my $s=IO::Select->new(); 
 $s->add(\*CALC_OUT);
 $s->add(\*CALC_ERR);
+
+sub calculate {
+	my $cmd=shift; 
+	print "Calculate $cmd\n"; 
+	my @ret; 
+	print CALC_IN $cmd."\n"; 
+	my $ready=0; 
+	while ($ready==0){
+		$ready=1;
+		my @calc_fh=$s->can_read(3);
+		foreach my $fh (@calc_fh) {
+			$ready=0; 
+			$result=<$fh>;
+			chomp($result);
+			if ($result eq "ready") { 
+				$ready=1; 
+			} else {
+				if ($result) {
+					push @ret, $result; 
+				}
+			}
+		}
+	}
+	return @ret; 
+}
+
+sub insert_pyvar {
+	my $var=shift; 
+	print "Insertin Variable $var\n"; 
+	my @ret=calculate($var); 
+	$ret[0]=~s/^\s+//g; 
+	$ret[0]=~s/\s+$//g; 
+	my ($val, $uncert)=split(/\+\/\-/, $ret[0]); 
+	if ($uncert) {
+		return "($val \\pm $uncer)"; 
+	} else {
+		return $val;
+	}
+}
 
 # fetch first ready 
 my $ready=0; 
@@ -38,31 +97,44 @@ while(<READ>) {
 		chomp;
 		my $n="\n";
 		if (/\r$/) { chop; $n="\r\n"; print "Dos Format detected\n";}
-		$_=$_."\n";
+		#$_=$_."\n";
 		# calc that 
-		print "Calculate $_"; 
-		print CALC_IN;
-		$ready=0;
-		while ($ready==0){
-			$ready=1;
-			my @calc_fh=$s->can_read(3);
-			foreach my $fh (@calc_fh) {
-				$ready=0; 
-				$result=<$fh>;
-				chomp($result);
-				if ($result eq "ready") { $ready=1; } else {
-					if ($result) {
-						print WRITE "%res ".$result.$n;
-					}
-				}
-			}
+		foreach my $result (calculate($_)) {
+			print WRITE "%res ".$result.$n; 
 		}
 	} elsif (/^%res/) {
 		# do nothing with result lines 
 	} else { print WRITE; }
 }
 
-unlink($ARGV[0]) or die "TMP"; 
-link(".$ARGV[0].tmp", $ARGV[0]) or die "TMP"; 
-unlink(".$ARGV[0].tmp") or die "TMP"; 
+close READ; 
+close WRITE; 
+
+unlink($texfile) or die "TMP"; 
+if (!$pdflatex) {
+	link(".$texfile.tmp", $texfile) or die "TMP"; 
+	unlink(".$texfile.tmp") or die "TMP"; 
+} else {
+	open(READ, "> .$texfile.tmp") or die "Write open fail"; 
+	open(WRITE, "< $texfile") or die "READ Opemn fail"; 
+	READ->autoflush(1); 
+	WRITE->autoflush(1); 
+
+	while(<READ>){
+		s/\@((\w|\d)+)\@/insert_pyvar($1)/gex; 
+		print WRITE; 
+	}
+
+	close READ; 
+	close WRITE; 
+
+	system("$pdflatex $texfile"); 
+	system("$pdflatex $texfile"); 
+	
+	if ( ! $leavetexfile) {
+		unlink($texfile) or die "TMP"; 
+		link(".$texfile.tmp", $texfile) or die "TMP"; 
+	}
+	unlink(".$texfile.tmp") or die "TMP"; 
+}
 
